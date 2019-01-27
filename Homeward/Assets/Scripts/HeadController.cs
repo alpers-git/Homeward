0e0328eed1aea019770bd48d7b3c87fdac375736
K <mustafaugurkaan@gmail.com>
2019-01-27 14:40:35 +0300

    Add Boss, cursor and splashscreen

---
+++ Homeward/Assets/Scripts/HeadController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+    public Animator animator;
+    float animCounter = 0f;
+    float animIndex = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("head", animIndex);
+
+        animIndex =  Mathf.Abs( Mathf.Sin(animCounter));
+        animCounter = animCounter + 0.1f;
+    }
+}
